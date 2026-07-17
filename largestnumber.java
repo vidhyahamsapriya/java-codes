62aaf4fd1c537ebedec320089c631e4dc4f45168
vidhyahamsapriya <vidhyabm116@gmail.com>
2026-07-17 14:52:42 +0530

    Create largestnumber.java

---
+++ largestnumber.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class LargerNumber{
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number1: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the number2: ");
+        int num2 = sc.nextInt();
+    
+           if (num1> num2){
+              System.out.println(" num1 is greater than num2");
+            }
+           else{
+              System.out.println("num2 is greater than num1");
+            }
+    
+}}
